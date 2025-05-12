be2fce34b4371bc32d285e4638fb18a9a2e86d11
ksyee <tjssud0647@gmail.com>
2025-05-12 11:27:43 +0900

    initial commit

---
+++ src/main/java/com/findit/FinditApplication.java
@@ -0,0 +1,13 @@
+package com.findit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/findit/FinditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.findit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
